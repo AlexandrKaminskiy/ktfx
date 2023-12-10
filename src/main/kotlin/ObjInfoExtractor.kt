97aad721362e389f313549c998c79e9bc1764d5a
AlexandrKaminskiy <kaminskiy@itopcase.ru>
2023-12-10 20:58:39 +0300

    Fix

--- src/main/kotlin/ObjInfoExtractor.kt
+++ src/main/kotlin/ObjInfoExtractor.kt
@@ -1,11 +1,6 @@
-import com.sun.javafx.iio.ImageFrame
-import com.sun.prism.Image
-import com.sun.prism.PhongMaterial
-import com.sun.prism.TextureMap
 import de.javagl.obj.Obj
 import de.javagl.obj.ObjReader
 import de.javagl.obj.ObjUtils
-import java.awt.Frame
 import java.io.FileInputStream
 
 
@@ -17,7 +12,6 @@ object ObjInfoExtractor {
         val obj = ObjUtils.convertToRenderable(
             ObjReader.read(inputStream)
         )
-        var tm = TextureMap(PhongMaterial.MapType.DIFFUSE)
         return obj
     }
 }
